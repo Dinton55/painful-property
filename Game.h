e116729ab965fca1a6cc0ce9f0c39e049841d9eb
Tom Pyne <tompyne@live.co.uk>
2016-10-06 12:47:18 +0100

    Game.h

---
+++ Game.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "GameBase.h"
+#include "timer.h"
+using namespace timer;
+#include "Player.h"
+#include "Obstacle.h"
+#include "random.h"
+
+enum GameState
+{
+	GAMESTATE_ACTIVE,
+	GAMESTATE_SPLASH,
+	GAMESTATE_RESET
+};
+
+using namespace random;
+
+const int NUM_OBSTACLES = 10;
+
+class Game :
+	public GameBase
+{
+	Obstacle* obstacle[NUM_OBSTACLES];
+	Timer *m_timer, *m_gameTime;
+	SDL_Rect m_cam;
+	Player *p;
+	GameState currState;
+	AWSprite splashSprite;
+
+	int m_currTime;
+	int m_winTime;
+public:
+	Game();
+	~Game();
+	void setup();
+	void logic();
+	void draw();
+	void onKeyPressed();
+
+	void updateCamera();
+	void playerPos();
+	void moveObstacles();
+	void checkForWin();
+	void performCollisions();
+	void updateScore();
+
+	void reset();
+
+	void displayScore();
+};
+
